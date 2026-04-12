5f9b1d72967c65d2ce5c56efe382a71ebddc1f83
alabenayed <alabenayed214@gmail.com>
2026-04-12 12:40:17 +0100

    Remove start-cluster.sh and stop-cluster.sh scripts as part of service refactoring

--- stop-cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Stopping opentrend containers..."
-docker-compose down
-
-echo "Stopping hadoop containers..."
-docker stop hadoop-master hadoop-worker1 hadoop-worker2
-
-echo "Done."
